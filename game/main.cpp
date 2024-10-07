7de0dd9e36b900a115d0083fd035a03da99e7836
ptizajar <ptizajar@gmail.com>
2024-10-07 22:36:45 +0300

    game ver1

---
+++ game/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int current_location = 1;
+
+void displayIntro() {
+
+}
+
+void displayCurrentLocation(int locationIndex) {
+
+}
+
+int main() {
+	cout << "aaa";
+}
